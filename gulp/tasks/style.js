0293f543f3c6f50e1a150d3f350dd68c1a338787
Royce <royce.yadav@gmail.com>
2018-06-06 16:16:05 -0400

    Added responsive images and media queries for hero

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -3,7 +3,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssVars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
   // console.log("Imagine a future stylesheet change.");
@@ -11,6 +12,10 @@ gulp.task('styles', function(){
   //send css through pipe to a destination
     //will create a new dir in temp folder
   return gulp.src('./app/assets/style/style.css')
-    .pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
+    .on('error',function(errorInfo){
+      console.log(errorInfo.toString()); //indicate error in human-friendly way
+      this.emit('end'); //won't stop gulp watch when error
+    })
     .pipe(gulp.dest('./app/temp/style'));
 });
