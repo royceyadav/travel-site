e3ae709982abef3017a52623bca678f34d146c07
Royce <royce.yadav@gmail.com>
2018-06-27 14:24:56 -0400

    Started working on header and nav

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -4,7 +4,8 @@ autoprefixer = require('autoprefixer'),
 cssVars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function(){
   // console.log("Imagine a future stylesheet change.");
@@ -12,7 +13,7 @@ gulp.task('styles', function(){
   //send css through pipe to a destination
     //will create a new dir in temp folder
   return gulp.src('./app/assets/style/style.css')
-    .pipe(postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssVars, nested, hexrgba, autoprefixer]))
     .on('error',function(errorInfo){
       console.log(errorInfo.toString()); //indicate error in human-friendly way
       this.emit('end'); //won't stop gulp watch when error
