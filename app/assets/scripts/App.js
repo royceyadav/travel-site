418bc75a7b04d17fd9764a31d0522b9f0ce000a5
Royce <royce.yadav@gmail.com>
2018-07-04 15:16:38 -0400

    Finished lesson 40 intro to webpack, finally

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -28,10 +28,16 @@
 // }
 
   //import in above constructor code from Person.js
+var $ = require('jquery');
 var Person = require('./modules/Person');
+
+// console.log(Person.exampleProperty);
+// Person.exampleFunction();
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
+
+$("h1").remove();
--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -5,3 +5,13 @@ function Person(fullName, favColour) {
     console.log("Hello, my name is " + this.name + " and my favourite colour is " + this.colour + ". ")
   }
 }
+
+module.exports = Person;
+/*
+console.log("Hello from Person.js");
+
+exports.exampleProperty = "Super magical example value";
+exports.exampleFunction = function(){
+  alert("This is an example");
+}
+*/
