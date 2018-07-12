d52c33339064244d13dc3431dbd989a37a13083b
Royce <royce.yadav@gmail.com>
2018-07-12 15:18:30 -0400

    created modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 // var revealOnScroll = new RevealOnScroll();
@@ -9,3 +10,4 @@ var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "86%");
 new RevealOnScroll($(".testimonial"), "61%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
