53c73c7b3c37a706f98257b6ca013380fc3e5ae8
Royce <royce.yadav@gmail.com>
2018-07-19 13:40:10 -0400

    adjusted waypoints to accommodate lazyload

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 var path = require('path');
 
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
+  },
   output: {
     path: path.resolve(__dirname, "./app/temp/scripts"),
-    filename: "App.js"
+    filename: "[name].js"
   },
 
   //use babel to convert new JS to old JS
