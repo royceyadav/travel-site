7258f8f13d71f1179f9185cc673ba3fddd1953f5
Royce <royce.yadav@gmail.com>
2018-07-19 15:17:45 -0400

    added 1px padding to sprites, modernizr stuff, sprite.js updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ require('./gulp/tasks/style');
 require('./gulp/tasks/watch');
 require('./gulp/tasks/sprites');
 require('./gulp/tasks/scripts');
+require('./gulp/tasks/modernizr');
 
 // var gulp = require('gulp'),
 // watch = require('gulp-watch'),
